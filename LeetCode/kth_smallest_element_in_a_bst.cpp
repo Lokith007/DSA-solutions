fbe877cb9dc8896ebf2bfd7bf3750d954a1331b5
Lokith <lokithk93@gmail.com>
2026-07-19 02:55:41 +0530

    solve: #230 Kth Smallest Element in a BST [Medium]

---
+++ LeetCode/kth_smallest_element_in_a_bst.cpp
@@ -0,0 +1,31 @@
+// Kth Smallest Element in a BST [Medium]
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void preOrderTraversal(TreeNode* root, vector<int> &val){
+        if(root == NULL){
+            return;
+        } 
+        val.push_back(root->val);
+        preOrderTraversal(root->left, val);
+        preOrderTraversal(root->right, val);      
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> v; 
+        preOrderTraversal(root, v);
+        sort(v.begin(), v.end());
+        return v[k-1];
+    }
+};
