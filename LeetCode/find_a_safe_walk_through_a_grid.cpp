5a2c0950233e7165a543753274085618ce335f0a
Lokith <lokithk93@gmail.com>
2026-07-21 00:26:04 +0530

    solve: #3558 Find a Safe Walk Through a Grid [Medium]

--- LeetCode/find_a_safe_walk_through_a_grid.cpp
+++ LeetCode/find_a_safe_walk_through_a_grid.cpp
@@ -3,49 +3,40 @@
 
 class Solution {
 public:
-    bool issafe(int i,int j,vector<vector<int>>&grid){
-        return i>=0 && j>=0 && i<grid.size() && j<grid[0].size();
+    bool issafe(int i,int j,vector<vector<int>>& grid){
+        return (i>=0 && j>=0 && i<size(grid) && j<size(grid[0]));
     }
-
-    bool solve(int i,int j,vector<vector<int>>&grid,int health,vector<vector<int>>&healthgrid){
-
-        if(health<1)
+    bool solve(int i,int j,vector<vector<int>>& grid,vector<vector<int>>& healthgrid,int health){
+        if(health<1){
             return false;
-
-        if(i==grid.size()-1 && j==grid[0].size()-1)
+        }
+        if(i==size(grid)-1 && j==size(grid[0])-1){
             return true;
-
+        }
         int dx[]={-1,0,1,0};
         int dy[]={0,1,0,-1};
-
         for(int k=0;k<4;k++){
-            int ni=i+dx[k];
-            int nj=j+dy[k];
-
+            int ni=i+dx[k],nj=j+dy[k];
             if(issafe(ni,nj,grid)){
-                int newHealth=health-grid[ni][nj];
-
-                if(newHealth>=1 && healthgrid[ni][nj]<newHealth){
-                    healthgrid[ni][nj]=newHealth;
-                    if(solve(ni,nj,grid,newHealth,healthgrid))
+                int newhealth=health-grid[ni][nj];
+                if(newhealth>=1 && healthgrid[ni][nj]<newhealth){
+                    healthgrid[ni][nj]=newhealth;
+                    if(solve(ni,nj,grid,healthgrid,newhealth)){
                         return true;
+                    }
                 }
             }
         }
-
         return false;
     }
-
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int n=grid.size(),m=grid[0].size();
-
-        health-=grid[0][0];
-        if(health<1)
+        int n=size(grid),m=size(grid[0]);
+        if(health<1){
             return false;
-
+        }
+        health-=grid[0][0];
         vector<vector<int>>healthgrid(n,vector<int>(m,-1));
         healthgrid[0][0]=health;
-
-        return solve(0,0,grid,health,healthgrid);
+        return solve(0,0,grid,healthgrid,health);
     }
 };
